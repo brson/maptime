e0eaca87aa4e6745c3eb4f937200e732ef709a6e
Brian Anderson <andersrb@gmail.com>
2019-09-29 16:10:19 +0800

    Add plotting

--- src/gnuplot.rs
+++ src/gnuplot.rs
@@ -1,3 +1,4 @@
+use gnuplot::{Figure, Caption, Color, AxesCommon};
 use crate::data::Commit;
 use std::time::Duration;
 use chrono::{DateTime, Utc};
@@ -19,11 +20,28 @@ pub struct Entry {
 }
 
 pub fn plot(data: PlotData, file: &Path) -> Result<(), Error> {
-    panic!()
+    let mut fg = Figure::new();
+    let mut fg2d = fg.set_terminal("svg size 600, 400", &file.to_str().ok_or(Error::PlotFile)?);
+    let mut fg2d = fg.axes2d();
+
+    for series in data.0 {
+        let x = series.values.iter().map(|e| e.commit.date.timestamp());
+        let y = series.values.iter().map(|e| e.duration);
+        fg2d.lines(x, y, &[Caption(&format!("{}+{}", series.profile.as_ref(), series.rebuild_type.as_ref()))]);
+        fg2d.set_x_time(true);
+    }
+
+    fg.show();
+
+    println!("plot in {}", file.display());
+
+    Ok(())
 }
 
 #[derive(Debug, Display)]
 pub enum Error {
+    #[display(fmt = "non-unicode plot file")]
+    PlotFile,
 }
 
 impl StdError for Error { }
