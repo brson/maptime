3a902aa56d354d2af7f4e34783ed2073cd83df6e
Brian Anderson <andersrb@gmail.com>
2019-09-30 04:41:50 +0000

    Better date formatting

--- src/gnuplot.rs
+++ src/gnuplot.rs
@@ -1,4 +1,4 @@
-use gnuplot::{Figure, Caption, Color, AxesCommon};
+use gnuplot::{Figure, PlotOption, Color, AxesCommon, LabelOption, AutoOption, TickOption};
 use crate::data::Commit;
 use std::time::Duration;
 use chrono::{DateTime, Utc};
@@ -21,16 +21,18 @@ pub struct Entry {
 
 pub fn plot(data: PlotData, file: &Path) -> Result<(), Error> {
     let mut fg = Figure::new();
-    let mut fg2d = fg.set_terminal("svg size 600, 400", &file.to_str().ok_or(Error::PlotFile)?);
+    fg.set_terminal("svg size 600, 400", &file.to_str().ok_or(Error::PlotFile)?);
     let mut fg2d = fg.axes2d();
+    fg2d.set_x_ticks(Some((AutoOption::Auto, 0)), &[TickOption::Format("%Y-%m-%d")], &[LabelOption::Rotate(270_f64)]);
 
     for series in data.0 {
         let x = series.values.iter().map(|e| e.commit.date.timestamp());
         let y = series.values.iter().map(|e| e.duration);
-        fg2d.lines(x, y, &[Caption(&format!("{}+{}", series.profile.as_ref(), series.rebuild_type.as_ref()))]);
+        fg2d.lines(x, y, &[PlotOption::Caption(&format!("{}+{}", series.profile.as_ref(), series.rebuild_type.as_ref()))]);
         fg2d.set_x_time(true);
     }
 
+    fg.echo(&mut std::io::stdout());
     fg.show();
 
     println!("plot in {}", file.display());
